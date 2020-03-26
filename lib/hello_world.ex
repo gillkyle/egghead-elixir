ac0781a24c0effe5a5b60c2be0a38928d78fbc65
Kyle Gill <kylerobertgill@gmail.com>
2020-03-26 11:44:24 -0600

    remove unused docs and example method

--- lib/hello_world.ex
+++ lib/hello_world.ex
@@ -1,18 +1,2 @@
 defmodule HelloWorld do
-  @moduledoc """
-  Documentation for HelloWorld.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HelloWorld.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
